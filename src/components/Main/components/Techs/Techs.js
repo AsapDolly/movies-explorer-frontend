9e04c996e59ca574c5ade23ce3dec097f2df7b43
dashi imekov <coradoburrows@yandex.ru>
2021-04-21 12:51:13 +0800

    no message

--- src/components/Main/components/Techs/Techs.js
+++ src/components/Main/components/Techs/Techs.js
@@ -7,25 +7,7 @@ function Techs() {
             <div className='techs__content'>
                 <h2 className='techs__title'>Технологии</h2>
                 <div className='about-project__description-area'>
-                    <div className='about-project__description-block'>
-                        <h2 className='about-project__description-title'>
-                            Дипломный проект включал 5 этапов
-                        </h2>
-                        <p className='about-project__description-paragraph'>
-                            Составление плана, работу над бэкендом, вёрстку, добавление функциональности и финальные
-                            доработки.
-                        </p>
-                    </div>
 
-                    <div className='about-project__description-block'>
-                        <h2 className='about-project__description-title'>
-                            На выполнение диплома ушло 5 недель
-                        </h2>
-                        <p className='about-project__description-paragraph'>
-                            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было соблюдать, чтобы успешно
-                            защититься.
-                        </p>
-                    </div>
                 </div>
 
                 <div className='techs__stack'>
